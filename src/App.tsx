8e69ef46b1baa99e25c928d47d30f0125f78244b
ramirogrisales <ramirogrisales@gmail.com>
2023-09-29 14:39:10 -0500

    fix: build commnadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route } from "wouter";
 import LoginButton from "./screens/Login/LoginScreen";
 import { useAuth0 } from '@auth0/auth0-react';
-import { LogOut } from "./screens/Login/LogOut";
 import { Home } from "./screens/Home/Home";
 import './App.css'
 import { NavBar } from "./components/NavBar/NavBar";
@@ -9,7 +8,6 @@ import Loading from "./components/Loading/Loading";
 
 const App = () => {
   const { isAuthenticated, user, isLoading } = useAuth0();
-  // console.log(user);
 return (
   <div>
       {
@@ -18,11 +16,6 @@ return (
         <Loading/> 
         :
         !isAuthenticated &&
-        // <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column', borderRadius: 5}}>
-        //   <div>
-        //     <h3>Welcome to PostApp Tita Media</h3>
-        //   </div>
-        // </div>
           <LoginButton/>
       }
       {
